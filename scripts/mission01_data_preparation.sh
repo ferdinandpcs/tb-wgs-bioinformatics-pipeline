ca9c57ee00db5a0d01aa702e9bc170362ddff93f
ferdinandpcs <ferdinand.prayogo.c@mail.ugm.ac.id>
2026-06-12 20:15:46 +0700

    Delete mission01_data_preparation.sh

--- scripts/mission01_data_preparation.sh
+++
@@ -1,6 +0,0 @@
-(wgs) ferdinandpcs@LAPTOP-OODCP5Q8:~$ cd ~/wgs/mission01
-(wgs) ferdinandpcs@LAPTOP-OODCP5Q8:~/wgs/mission01$ prefetch SRR37582408
-(wgs) ferdinandpcs@LAPTOP-OODCP5Q8:~/wgs/mission01$ fastq-dump --split-files SRR37582408/SRR37582408.sra
-(wgs) ferdinandpcs@LAPTOP-OODCP5Q8:~/wgs/mission01$ fastqc SRR37582408_1.fastq -o
-(wgs) ferdinandpcs@LAPTOP-OODCP5Q8:~/wgs/mission01$ fastqc SRR37582408_2.fastq -o
-(wgs) ferdinandpcs@LAPTOP-OODCP5Q8:~/wgs/mission01$ fastp -i SRR37582408_1.fastq -I SRR37582408_2.fastq -o sample_clean_1.fastq -O sample_clean_2.fastq --detect_adapter_for_pe --html fastp_report.html --json fastp_report.json
